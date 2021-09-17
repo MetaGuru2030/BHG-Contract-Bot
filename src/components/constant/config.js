086ff3c7fd20cf01c30183ed0d5c1bb3ccd9a34d
Hot-Gambler <Hot-Gambler>
2021-09-16 18:46:41 -0700

    update config

--- src/components/constant/config.js
+++ src/components/constant/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
-    NODE_URL: 'wss://quiet-lingering-pond.kovan.quiknode.pro/2124f88767ca59a449af38625328bcdb2d4b14a9/',
-    EXPLORER: 'https://kovan.etherscan.io/tx/',
-    WBNB: "0xd0A1E359811322d97991E03f863a0C30C2cF029C", // 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c
+    NODE_URL: 'wss://bsc-ws-node.nariox.org:443',
+    EXPLORER: 'https://bscscan.com/tx/',
+    WBNB: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c", // 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c
   }
   
   export default CONFIG
